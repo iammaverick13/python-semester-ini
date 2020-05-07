0db77aeffa2b155c39b7b51d90e71a1e42845d34
IMAMSODIK <40739580+IMAMSODIK@users.noreply.github.com>
2020-05-07 13:52:08 +0700

    menambahkan dictionary

--- struktur data.py
+++ struktur data.py
@@ -1,3 +1,7 @@
 my_list = ['imam sodik', 12, True]
 for i in my_list:
   print(i)
+
+biodata = {'name':'imam sodik', 'age':19, 'life':True}
+for key in biodata:
+  print(key+' : '+str(biodata[key]))
