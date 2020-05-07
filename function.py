49cef18d25478301150a9703515838c9aa1795a5
iammaverick13 <64954680+iammaverick13@users.noreply.github.com>
2020-05-07 17:12:30 +0700

    membuat file fungsi
    membuat file fungsi dengan isi program kalkulator sederhana

---
+++ function.py
@@ -0,0 +1,16 @@
+def additional(a, b):
+	return a + b
+
+def subtraction(a, b):
+	return a - b
+	
+def multiplication(a, b):
+	return a * b
+	
+def division(a, b):
+	return a / b
+	
+
+a = int(input('insert a number : ')
+b = int(input('insert the second number : ')
+print(additional(a, b))
